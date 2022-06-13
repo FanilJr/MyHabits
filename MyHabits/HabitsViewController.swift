607c5e4ee007cc99d0d3d1c207408e8ca174398d
Fanil_Jr <ethic91@icloud.com>
2022-06-13 12:33:47 +0500

    let's go

---
+++ MyHabits/HabitsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsViewController.swift
+//  MyHabits
+//
+//  Created by Fanil_Jr on 13.06.2022.
+//
+
+import UIKit
+
+class HabitsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
