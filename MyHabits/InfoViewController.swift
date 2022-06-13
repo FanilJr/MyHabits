92ac04b29d6e17401306e42a8811b98f97042cc2
Fanil_Jr <ethic91@icloud.com>
2022-06-13 14:02:03 +0500

    first

---
+++ MyHabits/InfoViewController.swift
@@ -0,0 +1,96 @@
+//
+//  InfoViewController.swift
+//  MyHabits
+//
+//  Created by Fanil_Jr on 13.06.2022.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    let scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        return scroll
+        
+    }()
+    
+    let contentView: UIView = {
+        let content = UIView()
+        content.translatesAutoresizingMaskIntoConstraints = false
+        return content
+        
+    }()
+    
+    let labelView: UILabel = {
+        let label = UILabel()
+        label.text = "Привычка за 21 день"
+        label.backgroundColor = .white
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+        
+    }()
+    
+    let infoLabel: UILabel = {
+        let label = UILabel()
+        label.text = #"""
+            Прохождение этапов, за которые за 21 день вырабатывается привычка, подчиняется следующему алгоритму:\#n
+            1. Провести 1 день без обращения к старым привычкам, стараться вести себя так, как будто цель, загаданная в перспективу, находится на расстоянии шага.\#n
+            2. Выдержать 2 дня в прежнем состоянии самоконтроля.\#n
+            3. Отметить в дневнике первую неделю изменений и подвести первые итоги — что оказалось тяжело, что — легче,
+            с чем еще предстоит серьезно бороться.\#n
+            4. Поздравить себя с прохождением первого серьезного порога в 21 день.
+            За это время отказ от дурных наклонностей уже примет форму осознанного преодоления и человек сможет больше работать в сторону принятия положительных качеств.\#n
+             5. Держать планку 40 дней. Практикующий методику уже чувствует себя освободившимся от прошлого негатива и двигается в нужном направлении с хорошей динамикой.\#n
+            6. На 90-й день соблюдения техники все лишнее из «прошлой жизни» перестает напоминать о себе, и человек, оглянувшись назад, осознает себя полностью обновившимся.\#n
+            Источник: psychbook.ru
+            """#
+        label.numberOfLines = 0
+        label.backgroundColor = .white
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+        
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setup()
+    }
+    
+    func setup() {
+        view.addSubview(scrollView)
+        [labelView, infoLabel].forEach { contentView.addSubview($0) }
+        scrollView.addSubview(contentView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            labelView.topAnchor.constraint(equalTo: contentView.topAnchor,constant: 22),
+            labelView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            
+            infoLabel.topAnchor.constraint(equalTo: labelView.bottomAnchor,constant: 16),
+            infoLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,constant: 16),
+            infoLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor,constant: -16),
+            infoLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
+            
+        ])
+    }
+  
+}
